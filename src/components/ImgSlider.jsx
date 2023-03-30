e0f723811fddcbf0ab41bdeb7f6a76cad2df462a
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-30 12:36:22 +0200

    viewers markup

--- src/components/ImgSlider.jsx
+++ src/components/ImgSlider.jsx
@@ -65,7 +65,7 @@ const Wrap = styled.div`
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
       rgb(0 0 0 / 73%) 0 16px 10px -10px;
     height: 100%;
-    transition: all 0.3s;
+    transition: all 0.25s;
 
     &:hover {
       border: 4px solid rgba(249, 249, 249, 0.8);
---
+++ src/components/Viewers.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+function Viewers() {
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" />
+      </Wrap>
+    </Container>
+  );
+}
+
+export default Viewers;
+
+const Container = styled.div`
+  margin-top: 38px;
+  display: grid;
+  padding: 30px 0 26px;
+  grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
+  grid-gap: 25px;
+`;
+
+const Wrap = styled.div`
+  border-radius: 10px;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0 16px 10px -10px;
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    border: 3px solid rgba(249, 249, 249, 0.8);
+  }
+`;
