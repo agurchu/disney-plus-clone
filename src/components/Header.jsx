e22810428430a5ef29f20c7c38c5dff925652b42
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-30 08:18:10 +0200

    header and home import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Nav>
+      <Logo src="" />
+      <NavMenu></NavMenu>
+    </Nav>
+  );
 }
 
 export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  color: white;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div``;
