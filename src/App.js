737c4ad1b61f55ad1b42d976278c2f6366fa25c4
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-30 08:18:56 +0200

    home and styled components install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
